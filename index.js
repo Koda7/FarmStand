c69d9a0390ad96a552601d657a72188b91b74336
Ruthvik Kodati <ruthvikkodati@gmail.com>
2023-11-28 18:08:16 -0500

    Added EJS Template to handle GET requests for the product route

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ async function main() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.get('/products', async (req, res) => {
+    const products = await Product.find({});
+    console.log(products);
+    res.render('products/index', {products});
+})
+
 app.get('/dog', (req, res) => {
     res.send('woof');
 })
